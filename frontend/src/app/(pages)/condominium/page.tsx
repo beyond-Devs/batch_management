7279c82f4017cd901ecdf0ee0e2ec5b90ef51661
beyond-Devs <bernardodasilvamorais1@gmail.com>
2024-10-18 15:13:34 +0100

    update

--- frontend/src/app/(pages)/condominium/page.tsx
+++ frontend/src/app/(pages)/condominium/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import { useState, useEffect } from "react";
 import createAxiosInstance from "@/helpers/global/services/axios/axios.instance";
@@ -10,6 +10,8 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, Di
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import nofound from '@/assets/midea/app/no-found.png';
+import Image from "next/image";
 
 const CondominiumsList = () => {
     const [condominiums, setCondominiums] = useState([]);
@@ -19,6 +21,8 @@ const CondominiumsList = () => {
     const [loading, setLoading] = useState(true);
     const [searchName, setSearchName] = useState('');
     const [searchLocation, setSearchLocation] = useState('');
+    const [isNoFound, setIsNoFound] = useState(false);
+
     const axios = createAxiosInstance();
 
     useEffect(() => {
@@ -43,6 +47,11 @@ const CondominiumsList = () => {
         );
         setFilteredCondominiums(filtered);
     }, [searchName, searchLocation, condominiums]);
+
+    useEffect(() => {
+        // Atualiza isNoFound baseado na quantidade de condomínios filtrados
+        setIsNoFound(filteredCondominiums.length === 0 && !loading);
+    }, [filteredCondominiums, loading]);
 
     const handleEdit = (condominium) => {
         setCurrentCondo(condominium);
@@ -76,10 +85,8 @@ const CondominiumsList = () => {
 
     return (
         <div className="w-11/12 mx-auto my-5 relative h-auto">
-            <div className="grid grid-cols-2 mb-4">
-                <h1 className="text-xl font-semibold text-justify dark:text-white">Lista de condomínios</h1>
-            </div>
-
+            {!isNoFound && (<h1 className="text-xl font-semibold text-justify dark:text-white mb-2">Lista de condomínios</h1>)}
+            {!isNoFound && (
             <div className="mb-4 flex gap-4">
                 <Input
                     type="text"
@@ -96,6 +103,7 @@ const CondominiumsList = () => {
                     className="w-full dark:border-neutral-600 dark:text-white"
                 />
             </div>
+            )}
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {loading ? (
@@ -106,58 +114,53 @@ const CondominiumsList = () => {
                             <Skeleton className="h-6 w-full text-gray-400 dark:text-dark-secondary" />
                         </Card>
                     ))
-                ) : filteredCondominiums.length > 0 ? (
-                    filteredCondominiums.map((condominium) => (
-                        <Card key={condominium.id} className="relative shadow-lg p-0 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl transition-all">
-                            <CardHeader>
-                                <CardTitle className="text-lg font-semibold dark:text-white">
-                                    {condominium.name}
-                                </CardTitle>
-                                <CardDescription className="text-sm dark:text-gray-400">
-                                    {condominium.location}
-                                </CardDescription>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <button className="absolute top-2 right-2 p-1 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700">
-                                            <MoreHorizontal className="w-5 h-5 text-gray-500 hover:text-gray-700 dark:text-gray-400" />
-                                        </button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent>
-                                        {condominium.streets && Array.isArray(condominium.streets)
-                                        ? 
-                                        <div></div>
-                                        : 
-                                        <Link href={`/condominium/manage?id=${condominium.id}`} >
-                                            <DropdownMenuItem>
-                                                <FileBarChart2 className="mr-2 w-4 h-4" />Montar esquema
-                                            </DropdownMenuItem>
-                                        </Link>
-                                        }
-                                        
-                                        <DropdownMenuItem onClick={() => handleEdit(condominium)}>
-                                            <Edit2 className="mr-2 w-4 h-4" /> Editar
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem onClick={() => handleDelete(condominium.id)}>
-                                            <Trash2 className="mr-2 w-4 h-4 text-red-600" /> Eliminar
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </CardHeader>
-                            <CardContent>
-                                <p className="dark:text-gray-200">
-                                    {condominium.streets && Array.isArray(condominium.streets)
-                                        ? `${condominium.streets.reduce((totalLotes, street) => totalLotes + (street.lots?.length || 0), 0)} lote(s) / Terreno (s)`
-                                        : 'Sem lotes cadastrados'}
-                                </p>
-                            </CardContent>
-                        </Card>
-                    ))
                 ) : (
-                    <div className="text-center dark:text-gray-400">
-                        <p>Não foi encontrado nenhum condomínio.</p>
-                    </div>
+                    <>
+                        {filteredCondominiums.map((condominium) => (
+                            <Card key={condominium.id} className="relative shadow-lg p-0 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl transition-all">
+                                <CardHeader>
+                                    <CardTitle className="text-lg font-semibold dark:text-white">{condominium.name}</CardTitle>
+                                    <CardDescription className="text-sm dark:text-gray-400">{condominium.location}</CardDescription>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <button className="absolute top-2 right-2 p-1 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700">
+                                                <MoreHorizontal className="w-5 h-5 text-gray-500 hover:text-gray-700 dark:text-gray-400" />
+                                            </button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <Link href={`/condominium/manage?id=${condominium.id}`}>
+                                                <DropdownMenuItem>
+                                                    <FileBarChart2 className="mr-2 w-4 h-4" />
+                                                </DropdownMenuItem>
+                                            </Link>
+                                            <DropdownMenuItem onClick={() => handleEdit(condominium)}>
+                                                <Edit2 className="mr-2 w-4 h-4" /> Editar
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem onClick={() => handleDelete(condominium.id)}>
+                                                <Trash2 className="mr-2 w-4 h-4 text-red-600" /> Eliminar
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="dark:text-gray-200">
+                                        {condominium.streets && Array.isArray(condominium.streets)
+                                            ? `${condominium.streets.reduce((totalLotes, street) => totalLotes + (street.lots?.length || 0), 0)} lote(s) / Terreno (s)`
+                                            : 'Sem lotes cadastrados'}
+                                    </p>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </>
                 )}
             </div>
+
+            {isNoFound && (
+                <div className="flex flex-col items-center justify-center text-center dark:text-gray-400 w-full">
+                    <Image src={nofound} alt="Imagem não encontrada" width={300} className="h-auto" />
+                    <h3 className="font-semibold text-xl">Nenhum condomínio encontrado!</h3>
+                </div>
+            )}
 
             <Link href="/condominium/create" passHref>
                 <Button className="fixed h-12 w-12 bottom-4 right-8 rounded-full p-3 text-white shadow-lg">
@@ -170,23 +173,17 @@ const CondominiumsList = () => {
                 <DialogContent className="sm:max-w-md bg-white">
                     <DialogHeader>
                         <DialogTitle>Editar Condomínio</DialogTitle>
-                        <DialogDescription>
-                            Preencha os dados abaixo para editar o condomínio.
-                        </DialogDescription>
+                        <DialogDescription>Preencha os dados abaixo para editar o condomínio.</DialogDescription>
                     </DialogHeader>
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            const form = e.target as HTMLFormElement;
-                            const formData = new FormData(form);
-                            const updatedData = Object.fromEntries(formData.entries());
-                            handleUpdate(updatedData);
-                        }}
-                    >
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        const form = e.target as HTMLFormElement;
+                        const formData = new FormData(form);
+                        const updatedData = Object.fromEntries(formData.entries());
+                        handleUpdate(updatedData);
+                    }}>
                         <div className="mt-4">
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-700">
-                                Nome do condomínio
-                            </label>
+                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-700">Nome do condomínio</label>
                             <input
                                 type="text"
                                 name="name"
@@ -196,9 +193,7 @@ const CondominiumsList = () => {
                             />
                         </div>
                         <div className="mt-4">
-                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-700">
-                                Localização
-                            </label>
+                            <label className="block text-sm font-medium text-gray-700 dark:text-gray-700">Localização</label>
                             <input
                                 type="text"
                                 name="location"
